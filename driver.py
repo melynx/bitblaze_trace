4f50a11dfa18465eb284af0e2c2b69057417e7f3
Chua Zheng Leong <melynx@gmail.com>
2018-02-03 20:11:50 +0800

    Add more instruction details

--- driver.py
+++ driver.py
@@ -7,6 +7,15 @@ def main():
     eh = trace.ReadInstruction()
     while eh:
         print(eh)
+        print(hex(eh.eflags), eh.cc_op, eh.df, eh.hflags, eh.ldt, eh.gdt, eh.tr, eh.idt)
+        for idx, op in enumerate(eh.ops):
+            print(op.type_str, hex(op.addr), hex(op.value), bin(op.tainted))
+            print(op.origin, op.offset, op.source_id, op.new_id)
+            if op.type_str == 'TMemLoc':
+                if idx < 3:
+                    print(eh.memregs[idx])
+                #print(eh.memregs[idx])
+        print('')
         eh = trace.ReadInstruction()
 
 if __name__ == '__main__':
